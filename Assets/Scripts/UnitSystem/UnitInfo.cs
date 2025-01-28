cd6c082890b4cfb90950281a6e252445a48d7e01
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-28 00:37:36 -0500

    Add max Health in UnitInfo so we can set it from inspector

--- Assets/Scripts/UnitSystem/UnitInfo.cs
+++ Assets/Scripts/UnitSystem/UnitInfo.cs
@@ -10,9 +10,11 @@ namespace UnitSystem
     [System.Serializable]
     public class UnitInfo : IIcon
     {
+        [SerializeField,Range(1,100)] private int _maxHealth;
         [SerializeField] private List<IActionInfo> _actions;
         
         [SerializeField] private VisualInformations _visualInformations;
         public VisualInformations VisualInformations => _visualInformations;
+        public int MaxHealth => _maxHealth;
     }
 }
