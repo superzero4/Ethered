6239f173275511a19ea143799eec0dff6152fca5
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-28 00:34:04 -0500

    BattleElement also need to ilmplement Health

--- Assets/Scripts/BattleSystem/IBattleElement.cs
+++ Assets/Scripts/BattleSystem/IBattleElement.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 namespace BattleSystem
 {
-    public interface IBattleElement : IIcon
+    public interface IBattleElement : IIcon, IHealth
     {
         public PositionData Position { get; }
         public ETeam Team { get; }
         public EPhase Phase => Position.Phase;
         public Vector2Int PositionVector => Position.Position;
+
         [Obsolete("Not very OOP, find another way")]
         public bool IsGround => Team == ETeam.None;
     }
