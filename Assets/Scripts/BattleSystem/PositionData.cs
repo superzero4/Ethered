7d882642122675bd86388934dd91ff0a17264ce4
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-29 15:02:41 -0500

    Extract intermediary wrapper class for 2D Position in case we'd use smthing else

--- Assets/Scripts/BattleSystem/PositionData.cs
+++ Assets/Scripts/BattleSystem/PositionData.cs
@@ -1,36 +1,69 @@
 using System;
 using Common;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace BattleSystem
 {
     [Serializable]
     public struct PositionData
     {
+        /// <summary>
+        /// Simple Wrapper class in case we'd like to use another indexer type
+        /// </summary>
+        [System.Serializable]
+        public struct PositionIndexer
+        {
+            [SerializeField]
+            public Vector2Int position;
+            public int x => position.x;
+            public int y => position.y;
+            public PositionIndexer(int x, int y)
+            {
+                position = new Vector2Int(x, y);
+            }
+
+            public static implicit operator Vector2Int(PositionIndexer positionIndexer)
+            {
+                return positionIndexer.position;
+            }
+
+            public static implicit operator PositionIndexer(Vector2Int position)
+            {
+                return new PositionIndexer { position = position };
+            }
+        }
+
         public int DistanceTo(PositionData other)
         {
             return Mathf.Abs(x - other.x) + Mathf.Abs(y - other.y);
         }
-        public PositionData(Vector2Int position, EPhase phase)
+
+        public PositionData(PositionIndexer position, EPhase phase)
         {
-            this.position = position;
+            this._position = position;
             this.phase = phase;
         }
-        public PositionData(int phase,int x,int y) : this(new Vector2Int(x,y),(EPhase)phase)
+
+        public PositionData(int x, int y, EPhase phase) : this(new PositionIndexer(x, y), phase)
         {
         }
-        [SerializeField]
-        public Vector2Int position;
-        [SerializeField]
-        public EPhase phase;
-        public int x => position.x;
-        public int y => position.y;
+
+        public PositionData(int phase, int x, int y) : this(x, y, (EPhase)phase)
+        {
+        }
+
+        [SerializeField] private PositionIndexer _position;
 
         public Vector2Int Position
         {
-            get => position;
-            set => position = value;
+            get => _position.position;
+            set => _position.position = value;
         }
+
+        [SerializeField] public EPhase phase;
+        public int x => Position.x;
+        public int y => Position.y;
 
         public EPhase Phase
         {
@@ -40,7 +73,7 @@ namespace BattleSystem
 
         public override string ToString()
         {
-            return $"({position.x},{position.y}:{Utils.PhaseToChar(phase)})";
+            return $"({Position.x},{Position.y}:{Utils.PhaseToChar(phase)})";
         }
     }
 }
