ae2acb8fc7a293145b4930df00b50a2e46e28bf5
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-29 17:14:57 -0500

    Integrate a protected method to be implemented and called through 2 public that also ensure life is in bound

--- Assets/Scripts/BattleSystem/Health.cs
+++ Assets/Scripts/BattleSystem/Health.cs
@@ -1,15 +1,21 @@
-using System;
-using UnityEngine;
-
 namespace BattleSystem
 {
     public interface IHealth
     {
-        int CurrentHealth { get; }
+        int CurrentHealth { get; protected set; }
         int MaxHealth { get; }
-        void TakeDamage(int damage);
+        protected void TakeDamageUncapped(int damage);
 
-        void Heal(int heal)
+        public void TakeDamage(int damage)
+        {
+            TakeDamageUncapped(damage);
+            if (CurrentHealth <= 0)
+                CurrentHealth = 0;
+            else if (CurrentHealth > MaxHealth)
+                CurrentHealth = MaxHealth;
+        }
+
+        public void Heal(int heal)
         {
             TakeDamage(-heal);
         }
