4217cdd8750167d969a0ff5db906bdcfb4fcb6dd
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-01-29 15:03:16 -0500

    Some trials that remains commented if they happen to be necessary to be present in the interface

--- Assets/Scripts/BattleSystem/IBattleElement.cs
+++ Assets/Scripts/BattleSystem/IBattleElement.cs
@@ -8,6 +8,14 @@ namespace BattleSystem
     {
         public PositionData Position { get; }
         public ETeam Team { get; }
+
+        //public EAllowedMovement allowedMovement { get; }
+        //public void CopyFrom(IBattleElement element)
+        //{
+        //    Position = element.Position;
+        //    Team = element.Team;
+        //    VisualInformations = element.VisualInformations;
+        //}
         public EPhase Phase => Position.Phase;
         public Vector2Int PositionVector => Position.Position;
 
