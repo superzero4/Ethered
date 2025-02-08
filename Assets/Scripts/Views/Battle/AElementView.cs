ac356cb9f3cfa8162c4defa127629be6b693ce2a
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-07 23:07:40 -0500

    Create base View Class to be a 3D View of a given IBattleElement

---
+++ Assets/Scripts/Views/Battle/AElementView.cs
@@ -0,0 +1,74 @@
+using BattleSystem;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public abstract class AElementView<T> : MonoBehaviour where T : BattleSystem.IBattleElement
+{
+    [FormerlySerializedAs("_renderer")] [Header("View")] [SerializeField] protected Renderer _mainRenderer;
+    [ReadOnly] [SerializeField] protected T _data;
+
+    [ReadOnly] [SerializeField] protected InfoUI _ui;
+
+    public void Init(T data, Grid grid, InfoUI ui)
+    {
+        _data = data;
+        _ui = ui;
+        Init(grid);
+        SetColor();
+    }
+
+    protected void SnapToCorrectPosition(Grid grid)
+    {
+        transform.position = grid.GetCellCenterWorld((Vector3Int)_data.Position.Position);
+    }
+    
+    public virtual Color PickColor()
+    {
+        Color color = Color.grey;
+        if (_data.Team == ETeam.Player)
+        {
+            switch (_data.Phase)
+            {
+                case EPhase.Normal: color = Color.blue; break;
+                case EPhase.Ethered: color = Color.cyan; break;
+                case EPhase.Both: color = Color.green; break;
+            }
+        }
+        else if (_data.Team == ETeam.Enemy)
+        {
+            switch (_data.Phase)
+            {
+                case EPhase.Normal: color = Color.red; break;
+                case EPhase.Ethered: color = Color.magenta; break;
+                case EPhase.Both: color = Color.yellow; break;
+            }
+        }else if (_data.IsGround)
+        {
+            switch(_data.Phase)
+            {
+                case EPhase.Normal: color = Color.white; break;
+                case EPhase.Ethered: color = Color.black; break;
+                case EPhase.Both: color = Color.grey; break;
+            }
+        }
+        return color;
+    }
+    protected virtual void SetColor(Color color)
+    {
+        _mainRenderer.material.color = color;
+    }
+    public void SetColor()
+    {
+        SetColor(PickColor());
+    }
+    protected virtual void Init(Grid grid)
+    {
+        SnapToCorrectPosition(grid);
+    }
+
+    public void UpdateUI()
+    {
+        _ui.SetInfo(_data);
+    }
+}
