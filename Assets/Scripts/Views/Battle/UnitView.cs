887d47bcafa11d875cc2b80eace528466ab16af5
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-08 21:18:40 -0500

    Implement UnitView correctly with SnapMovement

--- Assets/Scripts/Views/Battle/UnitView.cs
+++ Assets/Scripts/Views/Battle/UnitView.cs
@@ -5,30 +5,33 @@ using UnitSystem;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class UnitView : AElementView<Unit>
+namespace Views.Battle
 {
-    [Header("Unit")] [SerializeField] private HealthUI _healthUI;
-    [SerializeField] [ReadOnly] private Grid _grid;
-    protected override void Init(Grid grid)
+    public class UnitView : AElementView<Unit>
     {
-        Assert.IsNotNull(_healthUI, "No HealthUI assigned");
-        base.Init(grid);
-        _grid = grid;
-        _data.OnUnitMoves.AddListener(Move);
-        _data.OnUnitHealthChange.AddListener(UpdateHealth);
-        _data.OnUnitHealthChange.AddListener(_healthUI.UpdateHealth);
-    }
+        [Header("Unit")] [SerializeField] private HealthUI _healthUI;
+        [SerializeField] [ReadOnly] private Grid _grid;
 
-    private void UpdateHealth(UnitHealthData arg0)
-    {
-        //TDOO activate feedback using eventually using arg0.oldHealth value
-    }
+        protected override void Init(Grid grid)
+        {
+            Assert.IsNotNull(_healthUI, "No HealthUI assigned");
+            base.Init(grid);
+            _grid = grid;
+            Data.OnUnitMoves.AddListener(Move);
+            Data.OnUnitHealthChange.AddListener(UpdateHealth);
+            Data.OnUnitHealthChange.AddListener(_healthUI.UpdateHealth);
+        }
 
-    private void Move(UnitMovementData arg0)
-    {
-        //TODO animate movement from arg0.oldPosition to _unit.Position
-        SnapToCorrectPosition(_grid);
-        PickColor();
-    }
+        private void UpdateHealth(UnitHealthData arg0)
+        {
+            //TDOO activate feedback using eventually using arg0.oldHealth value
+        }
 
+        private void Move(UnitMovementData arg0)
+        {
+            //TODO animate movement from arg0.oldPosition to _unit.Position
+            SnapToCorrectPosition(_grid);
+            PickColor();
+        }
+    }
 }
