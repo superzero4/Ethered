2913e6319c6cbbbb7cb40a32944c2303fee2e552
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-08 21:21:12 -0500

    Create Selectable component and prefab

---
+++ Assets/Scripts/Views/Battle/Selection/Selectable.cs
@@ -0,0 +1,33 @@
+using BattleSystem.TileSystem;
+using Common.Events;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Views.Battle.Selection
+{
+    public class Selectable : MonoBehaviour
+    {
+        [SerializeField] private Collider _collider;
+        [SerializeField] private EnvironmentView _env;
+        [SerializeField]
+        private Transform _hintAnchor;
+
+        public Tile Tile => _env.Tile;
+        public SelectionEventData Selection => new(Tile.Base, Tile.Unit);
+
+        public Transform HintAnchor => _hintAnchor;
+
+        private void Awake()
+        {
+            Assert.IsTrue(_collider != null && _collider.isTrigger);
+        }
+
+        private void Start()
+        {
+            Assert.IsTrue(_env != null);
+            Assert.IsTrue(gameObject.layer == PhaseSelector.SelectableLayer);
+        }
+        
+    }
+}
