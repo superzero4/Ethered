56deca240db8904d50e55be06f80b6067034c25a
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-10 21:25:01 -0500

    Cretaed a SleectionState class to handle all Unit => action => target selection logic

--- Assets/Scripts/BattleSystem/Action.cs
+++ Assets/Scripts/BattleSystem/Action.cs
@@ -1,18 +1,31 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using BattleSystem.TileSystem;
+using NUnit.Framework;
 using UI.Battle;
 using UnitSystem;
 using UnitSystem.Actions.Bases;
 using UnityEngine;
-using OriginType = UnitSystem.Unit;//IBattleElement;
+using OriginType = UnitSystem.Unit; //IBattleElement;
+
 namespace BattleSystem
 {
     public class Action : IComparable<Action>
     {
+        //Intermediate class to define a group of targets, used in the abstract methods, we use intermediate class in case we need to use another data structure or have middle logic
         [SerializeField] private OriginType _origin;
         [SerializeField] private TargetCollection _targets;
+        public bool HasTargets => _targets != null && _targets.Count > 0;
+
+        [Obsolete(
+            "Private setter, targets shouldn't be accessed nor modiified directly, use methods that try to set them instead",
+            true)]
+        private TargetCollection Targets
+        {
+            set => _targets = value;
+            get => _targets;
+        }
+
         [SerializeField] private IActionInfo _info;
 
         public Action(OriginType origin, IActionInfo info)
@@ -29,20 +42,37 @@ namespace BattleSystem
         // ReSharper disable SimplifyConditionalTernaryExpression
         public bool CanExecute(Tilemap map)
         {
-            return (IsReady ?  _info.CanExecuteOnMap(_origin, _targets, map) : false);
+            return (IsReady ? _info.CanExecuteOnMap(_origin, _targets, map) : false);
         }
         // ReSharper restore SimplifyConditionalTernaryExpression
 
         private bool IsReady => _origin != null && _targets != null;
 
-        public bool TrySetTarget(OriginType origin, params IBattleElement[] target)
+        public bool TryAppendTarget(Unit origin, params IBattleElement[] target)
         {
-            if (_info.IsValidTarget(origin, target))
+            Assert.IsTrue(_targets != null, "Targets should be initialized before trying to append to them");
+            Assert.IsTrue(_origin != null || origin != null,
+                "Origin should be initialized before trying to append to them");
+            Assert.IsTrue((_origin == null && origin != null) || origin == _origin,
+                "Origin should be the same as the one used to initialize the action");
+            if (_origin == null)
+                _origin = origin;
+            if (_info.AreTargetsValid(_origin, target))
             {
                 _origin = origin;
-                _targets = new TargetCollection(target.ToList());
+                _targets.AddRange(target);
                 return true;
             }
+            else
+                return false;
+        }
+
+        public bool TrySetTarget(OriginType origin, params IBattleElement[] target)
+        {
+            _targets = new TargetCollection();
+            _origin = origin;
+            if (TryAppendTarget(_origin, target))
+                return true;
             else
             {
                 _origin = null;
---
+++ Assets/Scripts/Views/Battle/Selection/SelectionState.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using BattleSystem;
+using UnitSystem;
+using UnitSystem.Actions.Bases;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Serialization;
+
+namespace Views.Battle.Selection
+{
+    [System.Serializable]
+    public class SelectionState
+    {
+        [SerializeField] private Unit _origin;
+        [SerializeReference] private Action _action;
+        public bool CanSelectUnit => _origin == null;
+        public bool CanSelectAction => !CanSelectUnit && _action == null;
+        public bool CanSelectTarget => !CanSelectUnit && !CanSelectAction;
+
+        public SelectionState()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            _origin = null;
+            _action = null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <param name="force">We'll set selected unit even if another one is already selected, without having to perform a reset</param>
+        public void SetUnit(Unit unit, bool force = true)
+        {
+            Assert.IsTrue(force || CanSelectUnit, "Unit is already set and not forced");
+            _origin = unit;
+        }
+
+        public void SetAction(IActionInfo action)
+        {
+            Assert.IsTrue(CanSelectAction, "Unit is not set before trying to set action");
+            Assert.IsTrue(action != null && _origin.Info.Actions != null && _origin.Info.Actions.Contains(action),
+                $"Action is incorrect => Unit doesn't have action {action} in list {_origin.Info.Actions}");
+            _action = new Action(_origin, action);
+        }
+
+        public bool AppendTarget(Unit target)
+        {
+            Assert.IsTrue(!CanSelectTarget, "Unit or Action is not set before trying to set targets");
+            return _action.TryAppendTarget(_origin, target);
+        }
+        
+        public Action Confirm()
+        {
+            Assert.IsTrue(_origin != null, "Unit is not set");
+            Assert.IsTrue(_action != null, "Action is not set");
+            Assert.IsTrue(_action != null && _action.HasTargets, "Target is not set or empty");
+            return _action;
+        }
+    }
+}
