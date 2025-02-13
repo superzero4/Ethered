d128dd18d87c3833e64e8746fecf214781054972
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-13 17:55:21 -0500

    Create logic for gray scale sprite generation

--- Assets/Scripts/Common/VisualInformations.cs
+++ Assets/Scripts/Common/VisualInformations.cs
@@ -8,14 +8,48 @@ namespace Common.Visuals
     public struct VisualInformations
     {
         [SerializeField] private Sprite _sprite;
+        [SerializeField] private Sprite _grayScale;
         [SerializeField] private Color _color;
         [SerializeField] private string _name;
         [SerializeField] private string _description;
-        
-        
+
         public Sprite Sprite
         {
             get => _sprite;
+        }
+
+        public Sprite GrayScale
+        {
+            get
+            {
+                //We generate the grayscale sprite once when needed, stays null otherwise
+                if (_grayScale == null)
+                    _grayScale = GenerateGrayscale();
+                return _grayScale;
+            }
+        }
+
+        private Sprite GenerateGrayscale()
+        {
+            if (_sprite == null)
+                return null;
+            var spriteTexture = _sprite.texture;
+            Texture2D texture = new Texture2D(spriteTexture.width, spriteTexture.height);
+            texture.SetPixels(spriteTexture.GetPixels());
+            for (int i = 0; i < texture.width; i++)
+            {
+                for (int j = 0; j < texture.height; j++)
+                {
+                    Color color = texture.GetPixel(i, j);
+                    float gray = color.grayscale;
+                    texture.SetPixel(i, j, new Color(gray, gray, gray, color.a));
+                }
+            }
+
+            texture.Apply();
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+                new Vector2(0.5f, 0.5f));
+            return sprite;
         }
 
         public Color Color
@@ -34,14 +68,14 @@ namespace Common.Visuals
         {
             get => _description;
         }
-        
+
         [Button]
         private void ColorMaxAlpha()
         {
             _color.a = 1;
         }
+
         //Dynamic default that will be use as a real cohrent default when fallbacking from a null or C# default
         public static VisualInformations Default;
-
     }
 }
