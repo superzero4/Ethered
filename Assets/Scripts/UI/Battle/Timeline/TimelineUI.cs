2dcb4b2b8526e4c522ada0858106fb0ae679f234
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-15 12:39:38 -0500

    TimelineUI also reacts to timeline clear correctly

--- Assets/Scripts/UI/Battle/Timeline/TimelineUI.cs
+++ Assets/Scripts/UI/Battle/Timeline/TimelineUI.cs
@@ -7,20 +7,28 @@ using UnityEngine.Serialization;
 
 namespace UI.Battle
 {
-    public class TimelineUI : MonoBehaviour,IReset
+    public class TimelineUI : MonoBehaviour, IReset
     {
         [SerializeField] private TimelineMemberUI _memberPrefab;
         private DynamicHideAndShow<TimelineMemberUI> _memberPool;
+
         private void Awake()
         {
-            _memberPool = new DynamicHideAndShow<TimelineMemberUI>(_memberPrefab,10,transform);
+            _memberPool = new DynamicHideAndShow<TimelineMemberUI>(_memberPrefab, 10, transform);
         }
 
         public void OnTimelineMemberInserted(TimelineEventData t)
         {
-            var member = _memberPool.At(t.index);
-            member.gameObject.SetActive(true);
-            member.SetAction(t.action);
+            if (t.InsertIndex.HasValue)
+            {
+                var member = _memberPool.At(t.InsertIndex.Value);
+                member.gameObject.SetActive(true);
+                member.SetAction(t.Action);
+            }
+            else
+            {
+                _memberPool.Reset();
+            }
         }
 
         public void Reset()
