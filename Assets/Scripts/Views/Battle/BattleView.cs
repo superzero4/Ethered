eb8b6e0a9402dc48883f7be0380589095eb854a6
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-06 23:29:37 -0400

    Cleaned up slighlty more by introducing intermediary class for timeline related events

--- Assets/Scripts/Views/Battle/BattleView.cs
+++ Assets/Scripts/Views/Battle/BattleView.cs
@@ -1,8 +1,7 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using BattleSystem;
 using Common.Events;
-using Common.Events.Combat;
 using Common.Events.UserInteraction;
 using Common.GlobalFlow;
 using Common.Visuals;
@@ -27,14 +26,13 @@ namespace Views.Battle
         [Header("References")] [SerializeField, InfoBox("Just a big reference holder")]
         private BattleUI _ui;
 
-        [SerializeField] private GameObject _timelineInfoHints;
+        [SerializeField] private TimelineView _timelineView;
 
 
         [Header("Read Only")] [SerializeReference] [ReadOnly]
         private SelectionState _selectionState;
 
         [SerializeField] [ReadOnly] private Selector _selector;
-        [SerializeField] [ReadOnly] private IHints _timelineHints;
 
         [SerializeReference] [ReadOnly] private BattleSystem.Battle _battle;
 
@@ -47,13 +45,11 @@ namespace Views.Battle
         public void Init(BattleSystem.Battle battle, Selector selector)
         {
             //Creation
-            _timelineHints = _timelineInfoHints.GetComponent<IHints>();
             _selector = selector;
             _battle = battle;
             _selectionState = new SelectionState();
 
             //Event linkage
-            _battle.OnTimelineAction.AddListener(_ui.TimelineUI1.OnTimelineMemberInserted);
 
             //SelectionEvents
             _selector.Phase.Subscribe(_ui.PhaseUI);
@@ -62,22 +58,13 @@ namespace Views.Battle
             _selector.SelectionUpdated.AddListener(OnSelectionUpdated);
 
             //UI Events
-            _ui.Initialize(OnHoverAction);
+            _timelineView.Init(_ui.TimelineUI, _battle);
+            _ui.Initialize();
             _ui.ConfirmButton.AddListener(OnConfirmed);
             _ui.EndTurnButton.AddListener(() => StartCoroutine(_battle.NextTurn(_delay)));
             SetActionUIsCallback(OnActionClicked);
 
             StartCoroutine(_battle.InitNewTurn(_delay));
-        }
-
-        private void OnHoverAction(ActionEventData arg0)
-        {
-            var action = arg0.action;
-            if (action != null)
-                _timelineHints.HintMultiple(action.TargetsEnumerable.Select(t => t.Position)
-                    .Append(action.Origin.Position));
-            else
-                _timelineHints.Clear();
         }
 
         public bool CanAct(Unit unit) => unit != null && unit.Team == ETeam.Player && _battle.CanStillAct(unit);
@@ -107,18 +94,16 @@ namespace Views.Battle
                 _selector.AddResetables(actionUI);
                 actionUI.OnClick.AddListener(a => _ui.UnitUI.ResetActionUIs(actionUI));
                 actionUI.OnClick.AddListener(onClick);
-                //onClick.AddListener(e => Debug.LogWarning(" SELECTION Action selected: " + e));
             }
         }
 
         private void OnActionClicked(IActionInfo a)
         {
-            //TODO remove the IEnumerbale logic from selection and put it elsewhere
             var valid = _selectionState.SelectActionIfValid(a);
             if (valid)
             {
                 var targs = _battle.PossibleTargetPosition(_selectionState.Origin, a);
-                _selector.HintMultiple(targs);
+                _selector.Hints.HintMultiple(targs);
                 _selector.ShowHints = true;
             }
         }
@@ -129,8 +114,6 @@ namespace Views.Battle
             {
                 if (s.unit != null && CanAct(s.unit))
                 {
-                    //In theory, already set by the hover event so redundant but as a safe
-                    //_ui.UnitUI.SetUnit(s.unit,false);
                     _selectionState.SetUnit(s.unit, true);
                     _selector.ShowHints = false;
                 }
@@ -146,11 +129,6 @@ namespace Views.Battle
                     _ui.ConfirmButton.interactable = true;
                     //TODO Probably maintain a List of targets and not just a single LastTargetUI
                 }
-                else
-                {
-                    //Debug.LogWarning("SELECTION Target not valid");
-                    //TODO Show negative feedback showing target wasn't selected
-                }
             }
         }
 
@@ -159,16 +137,10 @@ namespace Views.Battle
             var action = _selectionState.Confirm();
             var confirmed = _battle.ConfirmAction(action);
             _selector.Reset();
-            if (confirmed)
-            {
-                Debug.Log("SELECTION Action confirmed");
-                //TODO Show positive feedback
-                //Timeline UI should have subscribed to timeline events and be update on it's own
-            }
-            else
+            if (!confirmed)
             {
                 //TODO Show cancel feedback
-            }
+            } // Else timeline UI should have subscribed to timeline events and be update on it's own
         }
     }
 }
