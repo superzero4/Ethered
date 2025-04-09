f2b402116b756830141159a9c7b62e04d1b57111
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-09 15:08:34 -0400

    Moved random brain to the AI folder

--- Assets/Scripts/UnitSystem/AI/RandomTryoutsBrain.cs
+++ Assets/Scripts/UnitSystem/AI/RandomTryoutsBrain.cs
@@ -4,19 +4,6 @@ using BattleSystem.TileSystem;
 
 namespace UnitSystem.AI.Dev
 {
-    public class OneBrainCollection : IBrainCollection
-    {
-        public OneBrainCollection(IBrain brain)
-        {
-            _brain = brain;
-        }
-        private IBrain _brain;
-        public IBrain GetBrain(EBrainType type)
-        {
-            return _brain;
-        }
-    }
-
     public class RandomTryoutsBrain : IBrain
     {
         private int _maxTryouts;
