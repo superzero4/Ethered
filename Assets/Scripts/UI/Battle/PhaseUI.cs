f34e48d419940b501a7cc203a6cf6c7df753cec0
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-21 02:10:06 -0400

    Refined the PhaseUI element

--- Assets/Scripts/UI/Battle/PhaseUI.cs
+++ Assets/Scripts/UI/Battle/PhaseUI.cs
@@ -14,10 +14,16 @@ namespace UI.Battle
         [SerializeField] private bool _showTargetPhase = true;
         [SerializeField] private UnityEngine.UI.Button _normal;
         [SerializeField] private UnityEngine.UI.Button _ethered;
+
+        [SerializeField] private UnityEngine.UI.Image[] _commons;
         [SerializeField] private UnityEvent _onClick = new();
+        private Color _normalColor;
+        private Color _etheredColor;
 
         public void Initialize()
         {
+            _normalColor = _normal.image.color;
+            _etheredColor = _ethered.image.color;
             _normal.onClick.AddListener(_onClick.Invoke);
             _ethered.onClick.AddListener(_onClick.Invoke);
         }
@@ -30,6 +36,9 @@ namespace UI.Battle
                     value = 1 - value;
                 _normal.image.color = SetAlpha(_normal.image.color, 1 - value);
                 _ethered.image.color = SetAlpha(_ethered.image.color, value);
+                var lerp = Color.Lerp(_normalColor, _etheredColor, value);
+                foreach (var image in _commons)
+                    image.color = lerp;
             }
         }
 
