dee3e14171dc9b9e16070a5c1b2c7bcad756edb6
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-21 01:56:53 -0400

    Added option to ivnert the toggled view of the pahse UI

--- Assets/Scripts/UI/Battle/PhaseUI.cs
+++ Assets/Scripts/UI/Battle/PhaseUI.cs
@@ -11,6 +11,7 @@ namespace UI.Battle
 {
     public class PhaseUI : MonoBehaviour, IPhaseView
     {
+        [SerializeField] private bool _showTargetPhase = true;
         [SerializeField] private UnityEngine.UI.Button _normal;
         [SerializeField] private UnityEngine.UI.Button _ethered;
         [SerializeField] private UnityEvent _onClick = new();
@@ -20,10 +21,13 @@ namespace UI.Battle
             _normal.onClick.AddListener(_onClick.Invoke);
             _ethered.onClick.AddListener(_onClick.Invoke);
         }
+
         public float Progress
         {
             set
             {
+                if (_showTargetPhase)
+                    value = 1 - value;
                 _normal.image.color = SetAlpha(_normal.image.color, 1 - value);
                 _ethered.image.color = SetAlpha(_ethered.image.color, value);
             }
