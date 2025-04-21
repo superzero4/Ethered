9bed5eb4bed15d83bcc2a62c8f3ae45cbadabbc8
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-21 03:41:49 -0400

    Added another mode for PhaseUI working only with the colors

--- Assets/Scripts/UI/Battle/PhaseUI.cs
+++ Assets/Scripts/UI/Battle/PhaseUI.cs
@@ -1,29 +1,35 @@
 using System;
+using System.Linq;
 using BattleSystem;
 using Common.Events;
 using Common.Events.UserInteraction;
 using Common.Events.UserInterface;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 using Views.Battle;
 
 namespace UI.Battle
 {
     public class PhaseUI : MonoBehaviour, IPhaseView
     {
-        [SerializeField] private bool _showTargetPhase = true;
+        [SerializeField] private bool _invert = true;
+
+        [SerializeField, Tooltip("Color lerp instead")]
+        private bool _alphaFade = false;
+
         [SerializeField] private UnityEngine.UI.Button _normal;
+        [SerializeField] private UnityEngine.UI.Image[] _normalImages;
         [SerializeField] private UnityEngine.UI.Button _ethered;
+        [SerializeField] private UnityEngine.UI.Image[] _etheredImages;
 
         [SerializeField] private UnityEngine.UI.Image[] _commons;
         [SerializeField] private UnityEvent _onClick = new();
-        private Color _normalColor;
-        private Color _etheredColor;
+        [SerializeField] Color _normalColor;
+        [SerializeField] Color _etheredColor;
 
         public void Initialize()
         {
-            _normalColor = _normal.image.color;
-            _etheredColor = _ethered.image.color;
             _normal.onClick.AddListener(_onClick.Invoke);
             _ethered.onClick.AddListener(_onClick.Invoke);
         }
@@ -32,11 +38,24 @@ namespace UI.Battle
         {
             set
             {
-                if (_showTargetPhase)
+                if (_invert)
                     value = 1 - value;
-                _normal.image.color = SetAlpha(_normal.image.color, 1 - value);
-                _ethered.image.color = SetAlpha(_ethered.image.color, value);
                 var lerp = Color.Lerp(_normalColor, _etheredColor, value);
+                if (_alphaFade)
+                {
+                    _normal.image.color = SetAlpha(_normal.image.color, 1 - value);
+                    _ethered.image.color = SetAlpha(_ethered.image.color, value);
+                }
+                else
+                {
+                    var ilerp = Color.Lerp(_etheredColor, _normalColor, value);
+                    foreach (var image in _normalImages)
+                        image.color = lerp;
+
+                    foreach (var image in _etheredImages)
+                        image.color = ilerp;
+                }
+
                 foreach (var image in _commons)
                     image.color = lerp;
             }
