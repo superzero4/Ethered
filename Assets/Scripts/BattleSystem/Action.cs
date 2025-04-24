14eeb1d7ad788a42fa5af154e354d46c1f61e981
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-24 01:35:25 -0400

    Recheck the target validity before checking if can execute to ensure dodges

--- Assets/Scripts/BattleSystem/Action.cs
+++ Assets/Scripts/BattleSystem/Action.cs
@@ -20,6 +20,7 @@ namespace BattleSystem
         public bool HasTargets => _targets != null && _targets.Count > 0;
         public IEnumerable<IBattleElement> TargetsEnumerable => _targets.Targets;
         public IBattleElement MainTarget => _targets.MainTarget;
+
         [Obsolete(
             "Private setter, targets shouldn't be accessed nor modiified directly, use methods that try to set them instead",
             true)]
@@ -41,12 +42,12 @@ namespace BattleSystem
             _info.Execute(_origin, _targets);
         }
 
-        // ReSharper disable SimplifyConditionalTernaryExpression
         public bool CanExecute(Tilemap map)
         {
-            return (IsReady ? _info.CanExecuteOnMap(_origin, _targets, map) : false);
+            //We check that the targets and been set, that they are still valid, and also that the current layout of the map is still correct
+            return IsReady && _info.AreTargetsValid(_origin, _targets.Targets.ToArray()) &&
+                   _info.CanExecuteOnMap(_origin, _targets, map);
         }
-        // ReSharper restore SimplifyConditionalTernaryExpression
 
         private bool IsReady => _origin != null && _targets != null;
 
@@ -58,6 +59,7 @@ namespace BattleSystem
         {
             get { return _targets.Count >= _info.NbTargets; }
         }
+
         /// <summary>
         /// 
         /// </summary>
